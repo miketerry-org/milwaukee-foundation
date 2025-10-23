f946321dc458442774f3e3bc62675f4193be16fd
Mike Terry <support@miketerry.org>
2025-10-23 02:52:36 -0500

    fully implmented logging and configuration

--- index.js
+++ index.js
@@ -4,8 +4,31 @@
 
 // import all necessary modules
 import assert from "./lib/assert.js";
+import Config from "./lib/config.js";
+import env from "./lib/env.js";
+import logLevels from "./lib/logLevels.js";
+import Log from "./lib/log.js";
+import LogTransport from "./lib/logTransport.js";
+import ConsoleLogTransport from "./lib/logTransportConsole.js";
+import FileLogTransport from "./lib/logTransportFile.js";
+import LibSQLLogTransport from "./lib/logTransportLibSQL.js";
+import MongoDBLogTransport from "./lib/logTransportMongoDB.js";
+import MySQLLogTransport from "./lib/logTransportMySQL.js";
+import PostgresLogTransport from "./lib/logTransportPostgres.js";
 import verify from "./lib/verify.js";
-// import env from "./lib/env.js";
-// import expect from "./lib/expect.js";
 
-// export { assert, env, expect };
+export {
+  assert,
+  Config,
+  env,
+  logLevels,
+  Log,
+  LogTransport,
+  ConsoleLogTransport,
+  FileLogTransport,
+  LibSQLLogTransport,
+  MongoDBLogTransport,
+  MySQLLogTransport,
+  PostgresLogTransport,
+  verify,
+};
